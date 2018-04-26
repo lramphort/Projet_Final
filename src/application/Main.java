a7db4046649595ca42f8f33b05024b0ff7678f57
li3 <yi.li@etu.univ-grenoble-alpes.fr>
2018-04-26 13:45:55 +0200

    界面
    实现了界面交互

--- src/application/Main.java
+++ src/application/Main.java
@@ -1,20 +1,39 @@
 package application;
 	
 import javafx.application.Application;
+import javafx.fxml.FXMLLoader;
 import javafx.stage.Stage;
+import javafx.scene.Parent;
 import javafx.scene.Scene;
+import javafx.scene.control.Button;
+import javafx.scene.control.TextArea;
+import javafx.scene.control.TextField;
 import javafx.scene.layout.BorderPane;
 
 
 public class Main extends Application {
-	@Override
+	
+	private Button _creation;
+	private TextArea _textarea;
+	private TextField _textf;
 	public void start(Stage primaryStage) {
 		try {
+			/*
 			BorderPane root = new BorderPane();
 			Scene scene = new Scene(root,400,400);
 			scene.getStylesheets().add(getClass().getResource("application.css").toExternalForm());
 			primaryStage.setScene(scene);
 			primaryStage.show();
+			*/
+			 // Read file fxml and draw interface.
+            Parent root = FXMLLoader.load(getClass()
+                    .getResource("/application/MyScene.fxml"));
+
+            primaryStage.setTitle("Application");
+            primaryStage.setScene(new Scene(root));
+            primaryStage.show();
+            
+            
 		} catch(Exception e) {
 			e.printStackTrace();
 		}
