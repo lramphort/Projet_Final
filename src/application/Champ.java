cab700850fa2576c649b3d45e5b285e2da29eaeb
Loic Ramphort <loic.ramphort@etu.univ-grenoble-alpes.fr>
2018-05-04 11:10:04 +0200

    loic

--- src/application/Champ.java
+++ src/application/Champ.java
@@ -12,12 +12,18 @@ public class Champ {
 	  * @param v
 	  * @param opt
 	  * @throws Exception 
-	  * @require val_not_null : opt && val.equals(null)
+	  * @require val_not_null : val.equals(null)
+	  * @ensure nom_valeur : this.nom == name && this.valeur == v
 	  */
 	 public Champ(String name, Object v) throws Exception{
-		 	if( name.equals(null) ) throw new Exception("name = null");
+		 	if( name.equals(null) )
+		 		{ throw new Exception("val_not_null");}
+		 	
 	    	this.nom = name;
 		    this.valeur = v;
+		    
+		    if( !(this.nom == name && this.valeur == v) )
+		    	{ throw new Exception("nom_valeur");}
 	    
 	 }
 	 
@@ -38,7 +44,12 @@ public class Champ {
 	 /**
 	  * modifie la valeur d'un champ
 	  * @param val
+	  * @ensure valCorrect : valeur ==  val
 	  */
-	 public void modifierValeur(Object val) {valeur=val;}
+	 public void modifierValeur(Object val) throws Exception{
+		 	valeur=val;
+		 	if(!(valeur ==  val))
+		 		{ throw new Exception("nom_valeur");}
+	 }
 	 
 }
